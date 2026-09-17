fc648afab94e34d295ca80db058ff993f96128ab
Phantom_StormX <Impichler@thewildcats.net>
2026-09-17 14:27:59 -0400

    added in art and finished conditionals

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# imports grab information from the corresponding files, and uses whatever you want from it
 import Welcome_screen
 import Space_and_Nature
 import pokemon
@@ -8,103 +9,132 @@ print(Welcome_screen.welcome_page)
 ticket = 5
 encounter = 0
 
+# main entry loop
 while True:
     ask = input(f'you have {ticket} tickets, that is enough to enter the gallery! would you like to enter? Y/N ')
-    if ask == 'Y' or ask == 'y':
-        print(f"""
-    welcome to the digital art gallery! 
-    you only have {encounter} encounters. 
-    here is a crash course of what this place is!
-    this is a awesome art gallery that consists of 4 different exhibits, or themes.
-    each exhibit has at least 4 art pieces...
+    if ask == 'y':
+        print(f""" 
+welcome to the digital art gallery! you only have {encounter} encounters. 
+here is a crash course of what this place is! this is an awesome art gallery that consists of 4 different exhibits, or themes. 
+each exhibit has at least 4 art pieces... 
 """)
-        encounter += 1
-        break
-    elif ask == "N" or ask == "n":
+        encounter += 1 # adds an encounter
+        break # stops the loop and moves on to the next piece of code
+    elif ask == 'n':
         print('then why did you even come here?? leave.')
-        break
+        exit() # closes the system
     else:
         print('invalid input, try again.')
 
-which_pokemon, which_mythology, which_space, which_animal = 0, 0, 0, 0
-
+# exhibit and art piece choice loop
 while True:
-    exhibit_entry = input('''
+    exhibit_entry = input(f'''
 If you want to access one of the exhibits, type and enter its corresponding number. 
+or if you want to leave, type and enter 5 
+
 Space and Nature exhibit [1] 
-Pokemon exhibit [2] 
-Mythology exhibit [3] 
-Animals exhibit [4] 
+Pokemon exhibit          [2] 
+Mythology exhibit        [3] 
+Animals exhibit          [4] 
+Exit Gallery             [5]
 ''')
 
     if exhibit_entry == '1':
-        which_space = input(''' 
-Awesome! my favorite exhibit! which art piece would you like to see? 
+        which_space = input(''' Awesome! my favorite exhibit! which art piece would you like to see? 
 saturn [1] 
 rotating galaxy [2] 
 northern lights [3] 
 mountains [4] 
 ''')
+        # if user chooses space, they have to choose which art piece
         if which_space == '1':
             print(Space_and_Nature.space_art1)
+            encounter += 1
         elif which_space == '2':
             print('work in progress sorry!')
+            encounter += 1
         elif which_space == '3':
             print(Space_and_Nature.space_art3)
+            encounter += 1
         elif which_space == '4':
             print(Space_and_Nature.space_art4)
-        break
+            encounter += 1
+        else:
+            print("Invalid art choice.")
 
     elif exhibit_entry == '2':
-        which_pokemon = input(''' 
-Awesome! which art piece would you like to see?
+        which_pokemon = input(''' Awesome! which art piece would you like to see? 
 meowth [1] 
 charmander [2] 
 squirtle [3] 
-ivysaur [4]''')
+ivysaur [4]
+''')
+        # if user chooses pokemon, they have to choose which art piece
         if which_pokemon == '1':
             print(pokemon.pokemon_art1)
+            encounter += 1
         elif which_pokemon == '2':
             print('work in progress sorry!')
+            encounter += 1
         elif which_pokemon == '3':
             print('work in progress sorry!')
+            encounter += 1
         elif which_pokemon == '4':
             print('work in progress sorry!')
-        break
+            encounter += 1
+        else:
+            print("Invalid art choice.")
 
     elif exhibit_entry == '3':
-        which_mythology = input(''' 
-Awesome! which art piece would you like to see?
+        which_mythology = input(''' Awesome! which art piece would you like to see? 
 dragon [1] 
 grim reaper [2] 
 gryphon [3] 
-mermaid [4] ''')
+mermaid [4] 
+''')
+        # if user chooses mythology, they have to choose which art piece
         if which_mythology == '1':
-            print('work in progress sorry!')
+            print(mythology.mythology_art1)
+            encounter += 1
         elif which_mythology == '2':
-            print('work in progress sorry!')
+            print(mythology.mythology_art2)
+            encounter += 1
         elif which_mythology == '3':
-            print('work in progress sorry!')
+            print(mythology.mythology_art3)
+            encounter += 1
         elif which_mythology == '4':
-            print('work in progress sorry!')
-        break
+            print(mythology.mythology_art4)
+            encounter += 1
+        else:
+            print("Invalid art choice.")
 
     elif exhibit_entry == '4':
-        which_animal = input(''' 
-Awesome! which art piece would you like to see?
+
+        which_animal = input(''' Awesome! which art piece would you like to see? 
 frog [1] 
+aardvark [2]
 cat [3] 
-amoeba [4] ''')
+amoeba [4] 
+''')
+        # if user chooses animal, they have to choose which art piece
         if which_animal == '1':
-            print('work in progress sorry!')
+            print(animals.animal_art1)
+            encounter += 1
         elif which_animal == '2':
-            print('work in progress sorry!')
+            print(animals.animal_art2)
+            encounter += 1
         elif which_animal == '3':
             print('work in progress sorry!')
+            encounter += 1
         elif which_animal == '4':
-            print('work in progress sorry!')
+            print(animals.animal_art4)
+            encounter += 1
+        else:
+            print("Invalid art choice.")
+
+    elif exhibit_entry == '5':
+        print("Thank you for visiting the digital art gallery! bye bye!")
         break
 
     else:
         print("invalid input, try again")
-
